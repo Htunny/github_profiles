ff64eb92162157ccc63ec2b6e2b16e92fc8d9e05
Andrew <andy_htun@hotmail.com>
2016-01-25 20:41:28 +0000

    display search results function added

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,3 +1,16 @@
 githubUserSearch.controller('GitUserSearchController', [function() {
+  var self = this;
+
+  self.doSearch = function() {
+    self.searchResult = {
+      "items": [
+        {
+          "login": "Htunny",
+          "avatar_url": "https://avatars2.githubusercontent.com/u/14794205?v=3&u=aaa59c90c369f802d6718c6ac6bcee66600ecc94&s=140",
+          "html_url": "https://github.com/Htunny"
+        }
+      ]
+    };
+  };
 
 }]);
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -11,4 +11,18 @@ describe('GitUserSearchController', function() {
     expect(ctrl.searchTerm).toBeUndefined();
   });
 
+  describe('when searching for a user', function() {
+    var items = [
+      {
+        "login": "Htunny",
+        "avatar_url": "https://avatars2.githubusercontent.com/u/14794205?v=3&u=aaa59c90c369f802d6718c6ac6bcee66600ecc94&s=140",
+        "html_url": "https://github.com/Htunny"
+      }
+    ];
+    it('displays search results', function() {
+        ctrl.searchTerm = 'hello';
+        ctrl.doSearch();
+        expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
 });
